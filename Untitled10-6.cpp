d5ccaa54761dcb4145f93d85078fd8a788b29f91
dung27-vi <duvi57836@gmail.com>
2025-11-04 14:17:48 +0700

    BTVN sau

---
+++ Untitled10-6.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+void bubbleSort(int arr[], int n) {
+    bool swapped;
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Hoán d?i arr[j] và arr[j+1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
+            }
+        }
+        if (swapped == false) {
+            break;
+        }
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    int arr[] = {5, 1, 4, 2, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    bubbleSort(arr, n);
+    std::cout << "Mang sau khi sap xep theo thu tu tang dan: ";
+    printArray(arr, n);
+    return 0;
+}
